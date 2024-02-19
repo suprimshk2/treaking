6fee0ca00d47b5262f46c159af80472721ff5d99
suprim shakya <suprim@noveltytechnology.com>
2024-02-19 16:07:35 +0545

    product delete issue fixes

--- src/features/product/mutations/index.tsx
+++ src/features/product/mutations/index.tsx
@@ -1,9 +1,14 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import {
+  InfiniteData,
+  useMutation,
+  useQueryClient,
+} from '@tanstack/react-query';
 import { enqueueSnackbar } from 'notistack';
 import { useBoundStore } from 'shared/stores/useBoundStore';
 import { infiniteProductKeys } from '../queries';
 import * as productAPI from '../api';
 import { IAdaptedproductSchema } from '../interfaces';
+import { IError } from 'shared/interfaces/http';
 
 export const useAddProductMutation = () => {
   const queryClient = useQueryClient();
@@ -104,25 +109,30 @@ export const useDeleteProductMutation = () => {
   return useMutation({
     mutationFn: (productId: string) => productAPI.deleteProduct(productId),
     onSuccess: (res) => {
-      enqueueSnackbar(res.message || 'Product added successfully', {
+      enqueueSnackbar(res.message || 'Product delete successfully', {
         variant: 'success',
       });
 
-      queryClient.setQueryData(
-        infiniteProductKeys.list(filters),
-        (old: any) => {
-          return {
-            ...old,
-            data: {
-              ...old.data,
-              count: old.data.count + 1,
-              rows: old.data.rows.filter(
-                (item) => item.productId !== res.data?.productId
-              ),
-            },
-          };
-        }
-      );
+      const queryKey = infiniteProductKeys.list({
+        ...filters,
+        // ...formatSortParam({
+        //   sortBy,
+        //   sortOrder,
+        // }),
+      });
+
+      const queryData: InfiniteData<any[]> | undefined =
+        queryClient.getQueryData(queryKey);
+
+      if (!queryData) {
+        return;
+      }
+      queryClient.invalidateQueries({ queryKey });
+    },
+    onError: (err: IError) => {
+      enqueueSnackbar(err.message || 'Error deleting product', {
+        variant: 'error',
+      });
     },
   });
 };
